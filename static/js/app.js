b1f81d49545256062d13f72c7dd6e8d28c16cc9f
unknown <al-keytree@outlook.com>
2023-04-15 14:17:56 -0400

    JS file to build and filter tabled data written

---
+++ static/js/app.js
@@ -0,0 +1,30 @@
+// import the data from data.js
+//  --- in the import line below
+const tableData = data;
+// reference the HTML table using D3
+var tbody = d3.select("tbody");
+// write a function to build the table
+function buildTable(data){
+    // first line clears the table. to clear previous users data.
+    tbody.html("");
+    // write a function to iterate through the object using forEach()
+    data.forEach((dataRow) => {
+        let row = tbody.append("tr");
+        // another forEach() function to loop through the row data(dataRow)
+        Object.values(dataRow).forEach((val) =>{
+            let cell = row.append("td");
+            cell.text(val);
+        });
+    });
+};
+function handleClick(){
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+    if (date){
+        filteredData = filteredData.filter(row => row.datetime === date)
+    }
+    buildTable(filteredData);
+};
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+buildTable(tableData);
